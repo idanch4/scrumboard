9fdf3f3dfadbc2fb6b12542bb50f1f8bc784e5c7
idanch4 <idanch4@gmail.com>
2019-11-20 19:00:22 +0200

    Editing data with django

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-from .api import ListApi, CardApi
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-    path('lists', ListApi.as_view()),
-    path('cards', CardApi.as_view()),
-    path('home', TemplateView.as_view(template_name="scrumboard/home.html")),
-]
+from .api import ListViewSet, CardViewSet
+
+
+router = DefaultRouter()
+router.register(r'lists', ListViewSet)
+router.register(r'cards', CardViewSet)
+
+urlpatterns = router.urls
