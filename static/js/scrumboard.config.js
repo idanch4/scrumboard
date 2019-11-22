048d3799ded1f3e8b6541766b7617a746a5cbc92
idanch4 <idanch4@gmail.com>
2019-11-22 19:45:29 +0200

    added frontend routing with NGroute

--- static/js/scrumboard.config.js
+++ static/js/scrumboard.config.js
@@ -1,10 +1,23 @@
 (function (){
     'use strict';
 
-    angular.module('scrumboard.demo').run(['$http', run]);
+    angular.module('scrumboard.demo')
+        .config(['$routeProvider', config])
+        .run(['$http', run]);
 
-    function run($http)
-    {
+    function config($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: '/static/html/scrumboard.html',
+                controller: 'ScrumboardController'
+            })
+            .when('/login', {
+                templateUrl: '/static/html/login.html'
+            })
+            .otherwise('/');
+    }
+
+    function run($http){
         $http.defaults.xsrfHeaderName = 'X-CSRFToken';
         $http.defaults.xsrfCookieName = 'csrftoken';
     }
